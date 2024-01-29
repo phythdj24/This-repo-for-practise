2983d2f63ae95a874404f1d79b57a47f4907b8ac
ATHARV DALAL <sangamdalal24@gmail.com>
2024-01-29 22:48:09 +0530

    Creating Login Auth

--- Rest/src/controllers/authentication.ts
+++ Rest/src/controllers/authentication.ts
@@ -2,6 +2,22 @@ import express from "express";
 import { createUser, getUserByEmail } from '../db/users';
 import { authentication, random } from "helpers";
 
+export const login = async(req: express.Request, res: express.Response) => {
+     
+
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+            return res.status(400);
+        }
+        
+    } catch (
+        
+    ) {
+        
+    }
+}
 
 export const register = async(req: express.Request, res: express.Response){
     try {
