b73ca2cf96cf02fb313cfac3f2c4713d435a2fec
ATHARV DALAL <sangamdalal24@gmail.com>
2024-02-03 22:52:21 +0530

    Validating Using ZOD

--- Hos/src/validators/auth.validtor.js
+++ Hos/src/validators/auth.validtor.js
@@ -1,30 +1,35 @@
-import { z } from "zod";
+// import { z } from "zod";
 
-// Creating Object Schema
-const signUpSchema = z.object({
-  name: z
-    .string({ required_error: "Name is Required" })
-    .trim()
-    .min(3, { message: "Name must be at least 3 Char" })
-    .max(255, { message: "Name must be more than 255 char" }),
+// // Creating Object Schema
+// const signUpSchema = z.object({
+//   name: z
+//     .string({ required_error: "Name is Required" })
+//     .trim()
+//     .min(3, { message: "Name must be at least 3 Char" })
+//     .max(255, { message: "Name must be more than 255 char" }),
 
-  doctorname: z
-    .string({ required_error: "Name is Required" })
-    .trim()
-    .min(3, { message: "Name must be at least 3 Char" })
-    .max(255, { message: "Name must be more than 255 char" }),
+//   doctorname: z
+//     .string({ required_error: "Name is Required" })
+//     .trim()
+//     .min(3, { message: "Name must be at least 3 Char" })
+//     .max(255, { message: "Name must be more than 255 char" }),
 
-  disease: z
-    .string({ required_error: "Name is Required" })
-    .trim()
-    .min(3, { message: "Name must be at least 3 Char" })
-    .max(255, { message: "Name must be more than 255 char" }),
+//   disease: z
+//     .string({ required_error: "Name is Required" })
+//     .trim()
+//     .min(3, { message: "Name must be at least 3 Char" })
+//     .max(255, { message: "Name must be more than 255 char" }),
 
-  address: z
-    .string({ required_error: "Name is Required" })
-    .trim()
-    .min(3, { message: "Name must be at least 3 Char" })
-    .max(255, { message: "Name must be more than 255 char" }),
-});
+//   address: z
+//     .string({ required_error: "Name is Required" })
+//     .trim()
+//     .min(3, { message: "Name must be at least 3 Char" })
+//     .max(255, { message: "Name must be more than 255 char" }),
+//   password: z
+//     .string({ required_error: "Password is Required" })
+//     .trim()
+//     .min(7, { message: "password must be at least 6 Char" })
+//     .max(1024, { message: "Name must be more than 1024 char" }),
+// });
 
-export default signUpSchema;
+// export default signUpSchema;
