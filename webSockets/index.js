fff86814153562c50c4c9ed71ea2564da8e9ec52
ATHARV DALAL <sangamdalal24@gmail.com>
2024-03-12 23:50:08 +0530

    socket connections

---
+++ webSockets/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import http from 'http'
+import path from "path";
+import { fileURLToPath } from 'url'; // Import fileURLToPath from 'url'
+
+const __filename = fileURLToPath(import.meta.url); // Get the current file's URL
+const __dirname = path.dirname(__filename); // Extract the directory name
+
+const app = express()
+
+const server = http.createServer(app)
+
+app.get('/',(req,res)=>{
+    return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // Use path.join to create the correct path to index.html
+})
+
+app.use(express.static(path.resolve('./public')))
+
+server.listen(9000, ()=>{
+    console.log(`server is Running On ${9000}`);
+})
