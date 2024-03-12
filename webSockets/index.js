6ea78df409ef076b77b7830d86ac33169fbc7714
ATHARV DALAL <sangamdalal24@gmail.com>
2024-03-12 23:50:27 +0530

    sockets

--- webSockets/index.js
+++ webSockets/index.js
@@ -2,13 +2,20 @@ import express from "express";
 import http from 'http'
 import path from "path";
 import { fileURLToPath } from 'url'; // Import fileURLToPath from 'url'
+import { Server } from "socket.io";
 
 const __filename = fileURLToPath(import.meta.url); // Get the current file's URL
 const __dirname = path.dirname(__filename); // Extract the directory name
 
 const app = express()
-
 const server = http.createServer(app)
+
+const io = new Server(server)
+
+//Socket Io
+io.on('connection', (client)=>{
+    
+})
 
 app.get('/',(req,res)=>{
     return res.sendFile(path.join(__dirname, 'public', 'index.html'));
